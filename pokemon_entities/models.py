41fa1b14627a7365d3d7f3e2400efb35fd1436c1
evgeniya35 <evgeniya35@yandex.ru>
2021-12-13 19:01:29 +0300

    add specs to PokemonEntity model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -13,8 +13,17 @@ class Pokemon(models.Model):
         return self.title
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.SET_NULL, null=True)
-    apperated_at = models.DateTimeField(null=True, default=None)
-    disapperated_at = models.DateTimeField(null=True, default=None)
-    lat = models.FloatField()
-    lon = models.FloatField()
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.SET_NULL, null=True, verbose_name='Покемон')
+    apperated_at = models.DateTimeField(null=True, default=None, verbose_name='Появляется')
+    disapperated_at = models.DateTimeField(null=True, default=None, verbose_name='Исчезает')
+    lat = models.FloatField(null=True)
+    lon = models.FloatField(null=True)
+    level = models.IntegerField(null=True, verbose_name='Уровень', help_text='Поле уровень')
+    health = models.IntegerField(null=True, verbose_name='Здоровье')
+    strength = models.IntegerField(null=True, verbose_name='Атака')
+    defence = models.IntegerField(null=True, verbose_name='Защита')
+    stamina = models.IntegerField(null=True, verbose_name='Выносливость')
+
+    def __str__(self) -> str:
+        return f'{self.pokemon} опции'
+
